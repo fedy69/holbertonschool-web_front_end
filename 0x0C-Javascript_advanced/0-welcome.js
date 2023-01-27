b28facc59f2bd34e21b5943568632055c4bb01a9
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-01-27 12:43:15 +0100

    welcome message

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,12 @@
+Copy code
+function welcome(firstName, lastName) {
+  let fullName = firstName + " " + lastName;
+  
+  function displayFullName() {
+    alert("Welcome " + fullName + "!");
+  }
+  
+  displayFullName();
+}
+
+welcome("Holberton", "School");
